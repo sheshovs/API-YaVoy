925640fcd47f41cb9d6acade686a90bbde32298e
Sergio Vargas <sheshovs@gmail.com>
2022-07-13 19:02:31 -0400

    Se agrego la documentacion

--- src/routes/product-service.js
+++ src/routes/product-service.js
@@ -3,9 +3,9 @@ const router = express.Router();
 //Import methods from controllers.
 const Product = require("../controllers/product");
 
-router.route("/product").post(Product.CreateProduct);
-router.route("/product").get(Product.GetAllProducts);
-router.route("/product/:id").delete(Product.DeleteProduct);
 router.route("/product/:id").get(Product.GetProductById);
+router.route("/product").get(Product.GetAllProducts);
+router.route("/product").post(Product.CreateProduct);
+router.route("/product/:id").delete(Product.DeleteProduct);
 
 module.exports = router;
